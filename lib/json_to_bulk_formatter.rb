6bff2b28fda2b147440d03876c6bb6381a9af255
KIRAryo1 <r.kira@grooves.co.jp>
2019-10-10 03:01:00 +0900

    Trim space and detect EOL

--- lib/json_to_bulk_formatter.rb
+++ lib/json_to_bulk_formatter.rb
@@ -3,16 +3,21 @@ output_filename = "data/hypnosys_character.json"
 
 txt = ""
 
-File.open(input_filename, "r") do |io|
+File.open(input_filename, 'r') do |io|
   txt = io.read
 end
 
 lines = txt.split("\n")
 
-File.open(output_filename, "w") do |io|
-  
+File.open(output_filename, 'w') do |io|
+  current_line = ''
   lines.each do |line|
-    io.puts line
-    puts line
+    if line.end_with?('EOL')
+      current_line += line.strip.delete('EOL')
+      io.puts current_line
+      current_line = ''
+    else
+      current_line += line.strip
+    end
   end
 end
